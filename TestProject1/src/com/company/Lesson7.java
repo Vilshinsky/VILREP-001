f4bd2f83af7b5e1051def1d22f9dc7aa37a83ef3
vilshinsky <paralon14>
2015-08-06 19:58:58 +0300

    lesson 7

---
+++ TestProject1/src/com/company/Lesson7.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Created by admin on 06.08.15.
+ */
+public class Lesson7 {
+    public static void main (String[] hw62) {
+        int[] b = {1,10,-20,30};
+        for (int a : b) {
+            System.out.println(a);
+        }
+        for(int i = 0; i < b.length; i++) {
+            System.out.println(b[i]);
+        }
+    }
+}
