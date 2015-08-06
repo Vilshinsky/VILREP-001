b6e78b5ba5b9815ba8f028a7255fa5de8e859610
vilshinsky <paralon14>
2015-08-06 20:01:12 +0300

    lesson 7

---
+++ TestProject1/src/com/company/LessonSix.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by admin on 04.08.15.
+ */
+public class LessonSix {
+    public static void main (String[] less6) throws InterruptedException {
+        Warmup1.sleepIn(true, false);
+        String a = "asdadsad";
+        System.out.println(a.length());
+        int [] b = {1,2,3,4,65,76};
+        System.out.println(b.length);
+        ArrayList<Integer> c = new ArrayList<Integer>();
+        System.out.println(c.size());                       //quantity of the parameters in the list
+        c.add(10);
+        System.out.println(c.size());
+        c.add(1);
+        c.add(5);
+        System.out.println(c.size());
+        System.out.println(c.get(1));
+        System.out.println(c.get(2));
+        c.remove(0);                                        //removing of the first (1,2,3) value in the list
+        System.out.println(c.size());
+        Integer[] c1 = {1,2,10,20};
+        c.addAll(Arrays.asList(c1));                        //mass creation of some values for the current list "c"
+        System.out.println(c.size());
+
+        System.setProperty("webdriver.chrome.driver", "C:\\Share\\chromedriver.exe");
+
+        ChromeDriver drvr1 = new ChromeDriver();
+        drvr1.get("http://google.com");
+        Thread.sleep(5000);
+        drvr1.quit();
+
+    }
+}
