f6c0cd6b686db2dcb757ba215eea3012c6b9229d
vilshinsky <paralon14>
2015-08-20 20:19:44 +0300

    lesson 8

---
+++ TestProject1/src/com/company/Lesson9.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * Created by admin on 18.08.15.
+ */
+public class Lesson9 {
+    public static void main (String[] less9) {
+
+    }
+}
