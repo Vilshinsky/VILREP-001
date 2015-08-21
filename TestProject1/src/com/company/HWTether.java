2786d937069ca54f4a339b824041976d2451fe2c
me <vilshinsky@gmail.com>
2015-08-21 13:00:42 +0300

    finished tether hw

--- TestProject1/src/com/company/HWTether.java
+++ TestProject1/src/com/company/HWTether.java
@@ -1,40 +1,36 @@
 package com.company;
 
+import javafx.scene.control.Tab;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- * Created by Omnigon on 8/20/2015.
- */
 public class HWTether {
-    public static void slp (long msec) {
-        try {
-            Thread.sleep(msec * 1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
-    public static WebElement findElement(String targetXPath, WebDriver wdr) {
-        for (int i = 0; i < 600; i++)    {
-            if (wdr.findElements(By.xpath(targetXPath)).size() > 0)
-            {
-                break;
-            }
-            slp(100);
-        }
-        return wdr.findElement(By.xpath(targetXPath));
-    }
     public static void main (String[] hwt) {
         System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
         ChromeDriver dvr = new ChromeDriver();
         dvr.get("https://stage.tether.to");
         WebElement logn = dvr.findElement(By.xpath("//*[@id='login']"));
-        WebElement pass = dvr.findElement(By.xpath("//*[@id='password']"));
-        pass.sendKeys("");
+        WebElement passw = dvr.findElement(By.xpath("//*[@id='password']"));
+        logn.sendKeys("");
+        passw.sendKeys("");
         WebElement logerr = dvr.findElement(By.xpath("//div[@ng-messages='loginForm.username.$error']/div[1]"));
-        System.out.println(logerr.getAttribute("value"));
+        System.out.println("Login: " + logerr.getText());
+        passw.sendKeys(Keys.TAB);
+        WebElement passer = dvr.findElement(By.xpath("//div[@ng-messages='loginForm.password.$error']/div[1]"));
+        System.out.println("Password: " + passer.getText());
+        passw.sendKeys("asd");
+        System.out.println("Password: " + passer.getText());
+        passw.sendKeys("fff");
+        System.out.println(dvr.findElements(By.xpath("//div[@ng-messages='loginForm.password.$error']/div[1]")).size());
+        logn.sendKeys("a");
+        System.out.println("Login: " + logerr.getText());
+        logn.sendKeys("qwe");
+        System.out.println(dvr.findElements(By.xpath("//div[@ng-messages='loginForm.username.$error']/div[1]")).size());
+        WebElement submit = dvr.findElement(By.xpath("//button[@class='submit-btn md-raised md-primary md-button md-default-theme']/span[1]"));
+        submit.click();
+
 
 
     }
