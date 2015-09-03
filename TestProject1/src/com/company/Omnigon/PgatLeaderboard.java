f37c7e6b9ced4c4006505fb2cbdfbad9283be750
me <vilshinsky@gmail.com>
2015-09-03 14:57:13 +0300

    css selectors

---
+++ TestProject1/src/com/company/Omnigon/PgatLeaderboard.java
@@ -0,0 +1,150 @@
+package com.company.Omnigon;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+
+/**
+ * Created by Omnigon on 9/3/2015.
+ */
+public class PgatLeaderboard {
+    public static void slp(long sec) {
+        try {
+            Thread.sleep(sec * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+//        System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+        WebDriver cssg = new FirefoxDriver();
+        cssg.get("http://www.pgatour.com/yahoo/leaderboard.html");
+        cssg.manage().window().setSize(new Dimension(1212, 766));
+        slp(10);
+
+        WebElement element = cssg.findElement(By.xpath("//*[@id='lbnFirst10LeaderboardContainer']/div[10]/table/tbody/tr[1]/td[5]/div/a[1]"));
+        Actions actions = new Actions(cssg);
+        actions.moveToElement(element);
+        actions.perform();
+        slp(10);
+
+        WebElement nextelement = cssg.findElement(By.cssSelector(".links-simple"));
+        Actions nextactions = new Actions(cssg);
+        nextactions.moveToElement(nextelement);
+        nextactions.perform();
+        slp(10);
+
+        //HEADER
+        cssg.findElement(By.cssSelector(".navbar-inner"));
+        cssg.findElement(By.cssSelector(".navbar-inner .brand"));
+        cssg.findElement(By.cssSelector(".brand-medium a:nth-child(2)"));
+        cssg.findElement(By.cssSelector(".header-btn"));
+        cssg.findElement(By.cssSelector(".nav-container .dropdown"));
+        cssg.findElement(By.cssSelector(".nav-container .dropdown li:nth-child(1)"));
+        cssg.findElement(By.cssSelector(".nav-container .dropdown li:nth-child(2)"));
+        cssg.findElement(By.cssSelector(".nav-container .dropdown li:nth-child(3)"));
+        cssg.findElement(By.cssSelector(".nav-container .dropdown li:nth-child(4)"));
+        cssg.findElement(By.cssSelector(".nav-container .dropdown li:nth-child(5)"));
+        cssg.findElement(By.cssSelector(".nav-container .dropdown li:nth-child(6)"));
+        cssg.findElement(By.cssSelector(".nav-container .dropdown li:nth-child(7)"));
+        cssg.findElement(By.cssSelector(".nav-container .dropdown li:nth-child(8)"));
+        cssg.findElement(By.cssSelector(".nav-container .dropdown li:nth-child(9)"));
+        cssg.findElement(By.cssSelector(".nav-container .dropdown li:nth-child(10)"));
+        cssg.findElement(By.cssSelector(".other-tours"));
+        cssg.findElement(By.cssSelector(".other-tours li:nth-child(1)"));
+        cssg.findElement(By.cssSelector(".other-tours li:nth-child(2)"));
+        cssg.findElement(By.cssSelector(".other-tours li:nth-child(3)"));
+        cssg.findElement(By.cssSelector(".other-tours li:nth-child(4)"));
+        cssg.findElement(By.cssSelector(".other-tours li:nth-child(5)"));
+        cssg.findElement(By.cssSelector(".login-button.log-in"));
+        cssg.findElement(By.cssSelector(".nav-search"));
+        cssg.findElement(By.cssSelector(".search-dropdown"));
+        cssg.findElement(By.cssSelector("#topNavSearch"));
+        cssg.findElement(By.cssSelector(".search-dropdown a"));
+
+        //HEADLIGHT PAGE
+        cssg.findElement(By.cssSelector(".tournament-banner.tournament-yahoo .banner"));
+        cssg.findElement(By.cssSelector(".info>.name a"));
+        cssg.findElement(By.cssSelector(".info-data"));
+        cssg.findElement(By.cssSelector(".hidden-small.sharing .link-box"));
+        cssg.findElement(By.cssSelector(".hidden-small.sharing .facebook"));
+        cssg.findElement(By.cssSelector(".hidden-small.sharing .twitter"));
+        cssg.findElement(By.cssSelector(".hidden-small.sharing .google-plus"));
+        cssg.findElement(By.cssSelector(".hidden-small.sharing .pinterest"));
+        cssg.findElement(By.cssSelector(".hidden-small.sharing .tumblr"));
+        cssg.findElement(By.cssSelector(".hidden-small.sharing .mailto"));
+        cssg.findElement(By.cssSelector(".tournament-banner.tournament-yahoo .logo"));
+
+        //AD1
+        cssg.findElement(By.cssSelector(".js-ad.ad-pack.topright.active"));
+
+        //SUBBANER
+        cssg.findElement(By.cssSelector(".subbanner"));
+        cssg.findElement(By.cssSelector(".course-info"));
+        cssg.findElement(By.cssSelector(".banner-weather"));
+        cssg.findElement(By.cssSelector(".banner-weather a"));
+        cssg.findElement(By.cssSelector(".broadcast-schedule li:nth-child(1)"));
+        cssg.findElement(By.cssSelector(".broadcast-schedule li:nth-child(1) img"));
+        cssg.findElement(By.cssSelector(".broadcast-schedule li:nth-child(1) .title"));
+        cssg.findElement(By.cssSelector(".broadcast-schedule li:nth-child(2)"));
+        cssg.findElement(By.cssSelector(".broadcast-schedule li:nth-child(2) img"));
+        cssg.findElement(By.cssSelector(".broadcast-schedule li:nth-child(2) .title"));
+        cssg.findElement(By.cssSelector(".broadcast-schedule li:nth-child(3)"));
+        cssg.findElement(By.cssSelector(".broadcast-schedule li:nth-child(3) img"));
+        cssg.findElement(By.cssSelector(".broadcast-schedule li:nth-child(3) .title"));
+
+        //LEADERBOARD
+        cssg.findElement(By.cssSelector(".tournament-leaderboard-new.rounds-4.no-rolex"));
+        cssg.findElement(By.cssSelector("#lbnLeaderboardHeaderContainer"));
+        cssg.findElement(By.cssSelector(".small-tab-selector .customSelect"));
+        cssg.findElement(By.cssSelector(".leaderboard-main"));
+        cssg.findElement(By.cssSelector(".leaderboard-main-content>div>div.ad-new"));
+        cssg.findElement(By.cssSelector(".ad-sticky"));
+        cssg.findElement(By.cssSelector(".ad-sticky .ad-block.large"));
+        cssg.findElement(By.cssSelector(".leaderboard-footer>.clearfix"));
+        cssg.findElement(By.cssSelector(".parsys.bottomAd1"));
+        cssg.findElement(By.cssSelector(".parsys.bottomAd2"));
+
+        //DONT MISS THIS SECTION
+        cssg.findElement(By.cssSelector(".container>.dontMissThisIParsys"));
+        cssg.findElement(By.cssSelector(".container>.dontMissThisIParsys p"));
+        cssg.findElement(By.cssSelector(".dontMissThisIParsys .item:nth-child(1) a"));
+        cssg.findElement(By.cssSelector(".dontMissThisIParsys .item:nth-child(2) a"));
+        cssg.findElement(By.cssSelector(".dontMissThisIParsys .item:nth-child(3) a"));
+        cssg.findElement(By.cssSelector(".dontMissThisIParsys .item:nth-child(4) a"));
+
+        //LINKS SIMPLE
+        cssg.findElement(By.cssSelector(".links-simple"));
+        cssg.findElement(By.cssSelector(".links-simple .item:nth-child(1)"));
+        cssg.findElement(By.cssSelector(".links-simple .item:nth-child(2)"));
+        cssg.findElement(By.cssSelector(".links-simple .item:nth-child(3)"));
+        cssg.findElement(By.cssSelector(".links-simple .item:nth-child(4)"));
+        cssg.findElement(By.cssSelector(".links-simple .item:nth-child(5)"));
+
+        //FOOTER
+        cssg.findElement(By.cssSelector(".footer-list ul"));
+        cssg.findElement(By.cssSelector(".footer-list li:nth-child(2)"));
+        cssg.findElement(By.cssSelector(".footer-list li:nth-child(4)"));
+        cssg.findElement(By.cssSelector(".footer-list li:nth-child(6)"));
+        cssg.findElement(By.cssSelector(".footer-list li:nth-child(7)"));
+        cssg.findElement(By.cssSelector(".footer-list li:nth-child(9)"));
+        cssg.findElement(By.cssSelector(".footer-list li:nth-child(11)"));
+        cssg.findElement(By.cssSelector(".footer-list li:nth-child(12)"));
+        cssg.findElement(By.cssSelector(".footer-copyright"));
+        cssg.findElement(By.cssSelector(".footerlConnectWith"));
+        cssg.findElement(By.cssSelector(".social-logo.social-logo-facebook"));
+        cssg.findElement(By.cssSelector(".social-logo.social-logo-twitter"));
+        cssg.findElement(By.cssSelector(".social-logo.social-logo-googleplus"));
+        cssg.findElement(By.cssSelector(".social-logo.social-logo-foursquare"));
+        cssg.findElement(By.cssSelector(".social-logo.social-logo-instagram"));
+        cssg.findElement(By.cssSelector(".social-logo.social-logo-pinterest"));
+        cssg.findElement(By.cssSelector(".social-logo.social-logo-youtube"));
+        cssg.findElement(By.cssSelector(".social-logo.social-logo-mobile"));
+        
+        cssg.quit();
+    }
+}
--- TestProject1/src/com/company/Omnigon/PresidentsCup.java
+++ TestProject1/src/com/company/Omnigon/PresidentsCup.java
@@ -27,38 +27,177 @@ public class PresidentsCup {
         cssg.manage().window().setSize(new Dimension(1212, 766));
         slp(10);
 
-        WebElement element = cssg.findElement(By.cssSelector(""));
+        WebElement element = cssg.findElement(By.cssSelector(".head-text"));
         Actions actions = new Actions(cssg);
         actions.moveToElement(element);
         actions.perform();
         slp(10);
 
-        WebElement nextelement = cssg.findElement(By.cssSelector(""));
+        WebElement nextelement = cssg.findElement(By.cssSelector(".middle.active .ad-chart-beat"));
         Actions nextactions = new Actions(cssg);
         nextactions.moveToElement(nextelement);
         nextactions.perform();
         slp(10);
 
-        //
+        //HEADER
+        cssg.findElement(By.cssSelector(".navbar-inner"));
+        cssg.findElement(By.cssSelector(".hidden-small.brand"));
+        cssg.findElement(By.cssSelector(".nav-container li:nth-child(1)"));
+        cssg.findElement(By.cssSelector(".nav-container li:nth-child(2)"));
+        cssg.findElement(By.cssSelector(".nav-container li:nth-child(3)"));
+        cssg.findElement(By.cssSelector(".banner img[src='/content/dam/pgatour/logos/navigation/presidents-banner.png']"));
+        cssg.findElement(By.cssSelector("a.citigroup"));
+        cssg.findElement(By.cssSelector("a.rolex"));
+        cssg.findElement(By.cssSelector(".user-menu.dropdown"));
 
-        //
+        //AD1
+        cssg.findElement(By.cssSelector(".js-ad.ad-pack.top .ad-chart-beat"));
 
-        //
+        //PAGE HEADLIGHT SECTION
+        cssg.findElement(By.cssSelector(".tournament-banner>.banner"));
+        cssg.findElement(By.cssSelector(".clearfix>[href='http://www.presidentscup.com']"));
+        cssg.findElement(By.cssSelector(".hidden-small .link-box"));
+        cssg.findElement(By.cssSelector(".hidden-small .facebook"));
+        cssg.findElement(By.cssSelector(".hidden-small .twitter"));
+        cssg.findElement(By.cssSelector(".hidden-small .google-plus"));
+        cssg.findElement(By.cssSelector(".hidden-small .pinterest"));
+        cssg.findElement(By.cssSelector(".hidden-small .tumblr"));
+        cssg.findElement(By.cssSelector(".hidden-small .mailto"));
+        cssg.findElement(By.cssSelector(".dates"));
+        cssg.findElement(By.cssSelector(".info-data [href='http://www.presidentscup.com']"));
+        cssg.findElement(By.cssSelector("[href='http://www.presidentscup.com'] img"));
 
-        //
+        //SUBBANER CLEARFIX
+        cssg.findElement(By.cssSelector(".subbanner-content.clearfix"));
+        cssg.findElement(By.cssSelector(".course-info"));
+        cssg.findElement(By.cssSelector(".more.popup-radio"));
+        cssg.findElement(By.cssSelector(".banner-weather"));
+        cssg.findElement(By.cssSelector(".banner-weather .more"));
+        cssg.findElement(By.cssSelector("ul.slider li:nth-child(1)"));
+        cssg.findElement(By.cssSelector("ul.slider li:nth-child(1) img"));
+        cssg.findElement(By.cssSelector("ul.slider li:nth-child(1) .title"));
+        cssg.findElement(By.cssSelector("ul.slider li:nth-child(1) .category"));
+        cssg.findElement(By.cssSelector("ul.slider li:nth-child(2)"));
+        cssg.findElement(By.cssSelector("ul.slider li:nth-child(2) img"));
+        cssg.findElement(By.cssSelector("ul.slider li:nth-child(2) .title"));
+        cssg.findElement(By.cssSelector("ul.slider li:nth-child(2) .category"));
+        cssg.findElement(By.cssSelector("ul.slider li:nth-child(3)"));
+        cssg.findElement(By.cssSelector("ul.slider li:nth-child(3) img"));
+        cssg.findElement(By.cssSelector("ul.slider li:nth-child(3) .title"));
+        cssg.findElement(By.cssSelector("ul.slider li:nth-child(3) .category"));
+        cssg.findElement(By.cssSelector(".banner-ad.ad-container"));
 
-        //
+        //TABS
+        cssg.findElement(By.cssSelector(".nav.nav-tabs.slim"));
+        cssg.findElement(By.cssSelector(".nav.nav-tabs.slim li:nth-child(2)"));
+        cssg.findElement(By.cssSelector(".nav.nav-tabs.slim li:nth-child(3)"));
+        cssg.findElement(By.cssSelector(".nav.nav-tabs.slim li:nth-child(4)"));
+        cssg.findElement(By.cssSelector(".nav.nav-tabs.slim li:nth-child(5)"));
+        cssg.findElement(By.cssSelector(".nav.nav-tabs.slim li:nth-child(6)"));
+        cssg.findElement(By.cssSelector(".nav.nav-tabs.slim li:nth-child(7)"));
+        cssg.findElement(By.cssSelector(".nav.nav-tabs.slim li:nth-child(8)"));
+        cssg.findElement(By.cssSelector(".nav.nav-tabs.slim li:nth-child(9)"));
+        cssg.findElement(By.cssSelector(".nav.nav-tabs.slim li:nth-child(1)"));
+        cssg.findElement(By.cssSelector(".wrapper .dropdown-menu"));
+        cssg.findElement(By.cssSelector(".dropdown.pull-right li:nth-child(1)"));
+        cssg.findElement(By.cssSelector(".dropdown.pull-right li:nth-child(2)"));
+        cssg.findElement(By.cssSelector(".dropdown.pull-right li:nth-child(3)"));
+        cssg.findElement(By.cssSelector(".dropdown.pull-right li:nth-child(4)"));
+        cssg.findElement(By.cssSelector(".dropdown.pull-right li:nth-child(5)"));
+        cssg.findElement(By.cssSelector(".dropdown.pull-right li:nth-child(6)"));
 
-        //
+        //COUNTDOWN SECTION
+        cssg.findElement(By.cssSelector(".head-text"));
+        cssg.findElement(By.cssSelector(".rolex-block"));
+        cssg.findElement(By.cssSelector(".countdown.section"));
+        cssg.findElement(By.cssSelector(".text.title.parbase p"));
+        cssg.findElement(By.cssSelector(".tournament-countdown .countdown"));
+        cssg.findElement(By.cssSelector(".countdown>div.countdown-section:nth-child(1)"));
+        cssg.findElement(By.cssSelector(".countdown>div.countdown-section:nth-child(2)"));
+        cssg.findElement(By.cssSelector(".countdown>div.countdown-section:nth-child(3)"));
+        cssg.findElement(By.cssSelector(".countdown>div.countdown-section:nth-child(4)"));
 
-        //
+        //US TEAM STANDINGS TABLE
+        cssg.findElement(By.cssSelector(".team_1.team"));
+        cssg.findElement(By.cssSelector(".team_1 .head"));
+        cssg.findElement(By.cssSelector(".team_1 [href='/teams.html']"));
+        cssg.findElement(By.cssSelector(".team_1 .team-leaders"));
+        cssg.findElement(By.cssSelector(".team_1 .ryders-data-table"));
+        cssg.findElement(By.cssSelector(".team_1 .footnote-qualifying"));
+        cssg.findElement(By.cssSelector(".team_1 .link-more-about>a"));
 
-        //
+        //INTERNATIONAL TEAM STANDINGS
+        cssg.findElement(By.cssSelector(".team_2.team"));
+        cssg.findElement(By.cssSelector(".team_2 .head"));
+        cssg.findElement(By.cssSelector(".team_2 [href='/teams.html']"));
+        cssg.findElement(By.cssSelector(".team_2 .team-leaders"));
+        cssg.findElement(By.cssSelector(".team_2 .ryders-data-table"));
+        cssg.findElement(By.cssSelector(".team_2 .footnote-qualifying"));
+        cssg.findElement(By.cssSelector(".team_2 .link-more-about>a"));
 
-        //
+        //CONNECT SECTION
+        cssg.findElement(By.cssSelector(".connect.section"));
+        cssg.findElement(By.cssSelector(".connect.section h2"));
+        cssg.findElement(By.cssSelector(".submodule.follow-us.clearfix"));
+        cssg.findElement(By.cssSelector(".submodule.follow-us.clearfix .social.social-facebook"));
+        cssg.findElement(By.cssSelector(".submodule.follow-us.clearfix .social.social-twitter"));
+        cssg.findElement(By.cssSelector(".submodule.follow-us.clearfix .social.social-instagram"));
+        cssg.findElement(By.cssSelector(".submodule.join-club.clearfix"));
+        cssg.findElement(By.cssSelector(".submodule.join-club.clearfix a"));
+        cssg.findElement(By.cssSelector(".submodule.contact-us.clearfix"));
+        cssg.findElement(By.cssSelector(".submodule.contact-us.clearfix a"));
 
-        //
+        //FEED SECTION
+        cssg.findElement(By.cssSelector(".feed.section"));
+        cssg.findElement(By.cssSelector(".tabbable-title"));
+        cssg.findElement(By.cssSelector(".tabbable-head.button-style.clearfix li:nth-child(3)"));
+        cssg.findElement(By.cssSelector(".tabbable-head.button-style.clearfix li:nth-child(4)"));
+        cssg.findElement(By.cssSelector(".tabbable-head.button-style.clearfix li:nth-child(5)"));
+        cssg.findElement(By.cssSelector(".grid .thumb:nth-child(1)"));
+        cssg.findElement(By.cssSelector(".grid .thumb:nth-child(1) img"));
+        cssg.findElement(By.cssSelector(".grid .thumb:nth-child(1) .thumb-info a"));
+        cssg.findElement(By.cssSelector(".grid .thumb:nth-child(2)"));
+        cssg.findElement(By.cssSelector(".grid .thumb:nth-child(2) img"));
+        cssg.findElement(By.cssSelector(".grid .thumb:nth-child(2) .thumb-info a"));
+        cssg.findElement(By.cssSelector(".grid .thumb:nth-child(3)"));
+        cssg.findElement(By.cssSelector(".grid .thumb:nth-child(4)"));
+        cssg.findElement(By.cssSelector(".grid .thumb:nth-child(4) img"));
+        cssg.findElement(By.cssSelector(".grid .thumb:nth-child(4) .thumb-info a"));
+        cssg.findElement(By.cssSelector(".grid .thumb:nth-child(5)"));
+        cssg.findElement(By.cssSelector(".grid .thumb:nth-child(5) img"));
+        cssg.findElement(By.cssSelector(".grid .thumb:nth-child(5) .thumb-info a"));
+        cssg.findElement(By.cssSelector(".grid .thumb:nth-child(6)"));
+        cssg.findElement(By.cssSelector(".grid .thumb:nth-child(6) img"));
+        cssg.findElement(By.cssSelector(".grid .thumb:nth-child(6) .thumb-info a"));
+        cssg.findElement(By.cssSelector(".response-btn-area.more .response-show-more"));
 
-//        cssg.quit();
+        //AD2
+        cssg.findElement(By.cssSelector(".middle.active .ad-chart-beat"));
+
+        //WEATHER HUB
+        cssg.findElement(By.cssSelector(".weather.section"));
+        cssg.findElement(By.cssSelector(".weather.section .module-header a"));
+        cssg.findElement(By.cssSelector(".text-right"));
+        cssg.findElement(By.cssSelector(".weather.section .temp"));
+        cssg.findElement(By.cssSelector(".weather.section .cond"));
+        cssg.findElement(By.cssSelector(".weather.section .wind"));
+        cssg.findElement(By.cssSelector(".weather.section .humidity"));
+        cssg.findElement(By.cssSelector(".weather.section .results-links a"));
+
+        //FOOTER
+        cssg.findElement(By.cssSelector(".footerNavigationPCUP"));
+        cssg.findElement(By.cssSelector(".footer-list li:nth-child(1) a"));
+        cssg.findElement(By.cssSelector(".footer-list li:nth-child(2) a"));
+        cssg.findElement(By.cssSelector(".footer-list li:nth-child(3) a"));
+        cssg.findElement(By.cssSelector(".footer-list li:nth-child(4) a"));
+        cssg.findElement(By.cssSelector(".footer-list li:nth-child(5) a"));
+        cssg.findElement(By.cssSelector(".footer-list li:nth-child(6) a"));
+        cssg.findElement(By.cssSelector(".footer-list li:nth-child(7) a"));
+        cssg.findElement(By.cssSelector(".footer-list li:nth-child(8) a"));
+        cssg.findElement(By.cssSelector(".footer-copyright p"));
+        cssg.findElement(By.cssSelector(".social-logo-facebook"));
+        cssg.findElement(By.cssSelector(".social-logo-twitter"));
+        cssg.findElement(By.cssSelector(".social-logo-instagram"));
+        cssg.quit();
     }
 }
